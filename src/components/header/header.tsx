65bee227c56988c24ac16cc0e4e1fc66f3230041
Gabriel Diaz <diazhernandezgabriel2001@gmail.com>
2024-12-15 15:30:24 -0300

    feat: add new condition

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -15,16 +15,19 @@ export function Header({ user }: { user: User | null }) {
     <header className="w-full border-b-2 border-t-0 border-slate-200 p-4 bg-white shadow-md">
       <nav className="max-w-3xl mx-auto">
         <ul className="flex gap-12 justify-center">
-          <li>
-            <Link
-              to="/qr-reader"
-              className={`text-lg font-semibold hover:text-blue-500 transition duration-300 ${isActive(
-                '/qr-reader'
-              )}`}
-            >
-              Lector QR
-            </Link>
-          </li>
+          {user?.role === 'tecnico' && (
+            <li>
+              <Link
+                to="/qr-reader"
+                className={`text-lg font-semibold hover:text-blue-500 transition duration-300 ${isActive(
+                  '/qr-reader'
+                )}`}
+              >
+                Lector QR
+              </Link>
+            </li>
+          )}
+
           {user &&
             user.role === 'admin' && ( // Verifica si el usuario es admin
               <li>
